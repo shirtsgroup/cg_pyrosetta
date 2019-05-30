0d3cd2f5a9709513b1095019d83b7ca2fffe3404
tlfobe <lennyfobe@gmail.com>
2019-05-30 11:13:11 -0600

    added graphing script for fold_from_sequence example

--- examples/fold_from_sequence/fold_from_sequence.py
+++ examples/fold_from_sequence/fold_from_sequence.py
@@ -1,13 +1,12 @@
 # This example file will be used to show how to generally use the CG_folding.FoldingAlgorithm object
-
 import cg_pyrosetta
-from cg_pyrosetta.CG_folding import pyrosetta
+from cg_pyrosetta.CG_folding import pyrosetta as pyrosetta
 
 # list of sequences of several CG models [CG11*5, CG21*5, CG31*5, mixed]
-sequences = ['X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]',
-          'X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]',
-          'X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]',
-          'X[CG11]X[CG21]X[CG31]X[CG21]X[CG11]']
+sequences = ['X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]X[CG11]',
+          'X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]X[CG21]',
+          'X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]X[CG31]',
+          'X[CG11]X[CG21]X[CG31]X[CG21]X[CG11]X[CG11]X[CG21]X[CG31]X[CG21]X[CG11]X[CG11]X[CG21]X[CG31]X[CG21]X[CG11]']
 
 names = ['CG11', 'CG21', 'CG31', 'mixed']
 
@@ -15,27 +14,30 @@ names = ['CG11', 'CG21', 'CG31', 'mixed']
 kt_i = 100
 kt_anneal = [kt_i*(0.9)**i for i in range(50)]
 
-for i in range(len(sequences)):
-    # first generate a folded structure using CGFoldingAlgorithm
-    folding_object = cg_pyrosetta.CG_folding.CGFoldingAlgorithm(sequences[i])
+for rep in range(5):
 
-    # If running PyMOL this will ensure structure output during MC simulation
-    # 'default' is the folding algorithm selected
-    # this algorithn consists of:
-    # 10x CGSmallMover
-    # 10x CGShearMober
-    # 10x MinMover
-    # MC evaluation
- 
-    folding_object.add_folding_move('default', folding_object.pymol)
-    
-    # Adding an angle mover to this folding algorithm
-    angle_mover = cg_pyrosetta.CG_movers.CGSmallAngleMover(folding_object.pose)
-    repeat_angle_mover = pyrosetta.RepeatMover(angle_mover, 10)
-    folding_object.add_folding_move('default', repeat_angle_mover)
+    for i in range(len(sequences)):
+        # first generate a folded structure using CGFoldingAlgorithm
+        folding_object = cg_pyrosetta.CG_folding.CGFoldingAlgorithm(sequences[i])
 
-    # Runs a folding MC simulation with 200 repeats of the 'default' folder at each kt
-    folding_object.run_anneal_fold('default', 2000, kt_anneal)
+        # If running PyMOL this will ensure structure output during MC simulation
+        # 'default' is the folding algorithm selected
+        # this algorithn consists of:
+        # 10x CGSmallMover
+        # 10x CGShearMober
+        # 10x MinMover
+        # MC evaluation
+        folding_object.scorefxn.set_weight(pyrosetta.rosetta.core.scoring.mm_twist, 0.1)
+        folding_object.add_folding_move('default', folding_object.pymol)
+        
+        # Adding an angle mover to this folding algorithm
+        angle_mover = cg_pyrosetta.CG_movers.CGSmallAngleMover(folding_object.pose)
+        angle_mover.angle = 30
+        repeat_angle_mover = pyrosetta.RepeatMover(angle_mover, 10)
+        folding_object.add_folding_move('default', repeat_angle_mover)
 
-    # Dump the lowest energy structure from the MC simulation
-    folding_object.mc.lowest_score_pose().dump_pdb(names[i]+'_example.pdb')
+        # Runs a folding MC simulation with 200 repeats of the 'default' folder at each kt
+        folding_object.run_anneal_fold('default', 5000, kt_anneal)
+
+        # Dump the lowest energy structure from the MC simulation
+        folding_object.mc.lowest_score_pose().dump_pdb('outputs/'+names[i]+'_example_angles_'+str(rep)+'.pdb')
