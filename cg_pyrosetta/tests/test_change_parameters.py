165cb81329b9e382218303169a2b696e17bed429
tlfobe <lennyfobe@gmail.com>
2019-10-29 16:30:10 -0600

    added importlib.reload to test reloading energy functions, still not working

--- cg_pyrosetta/tests/test_change_parameters.py
+++ cg_pyrosetta/tests/test_change_parameters.py
@@ -3,6 +3,9 @@ import pyrosetta
 import os
 import pytest
 import numpy as np
+import importlib
+
+pyrosetta.init()
 
 current_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(current_path, '../data')
@@ -10,7 +13,7 @@ pyrosetta_top_path = os.path.join(data_path, '../../PyRosetta4.modified')
 
 def test_change_atom_parameters():
     cg_pyrosetta.change_parameters.changeAtomParameters(
-                        {'CG1':['X', 10.0, 10.2, 1.0, 1.5, 23.7]})
+                        {'CG1':['X', 1.0, 0.2, 1.0, 1.5, 23.7]})
 
     # check local data dir (mm + fa_standard)
     with open(os.path.join(data_path, "atom_type_sets", "atom_properties.txt"), 'r') as fhandle:
@@ -21,8 +24,8 @@ def test_change_atom_parameters():
 
     # check pyrosetta data 
     
-    atom_prop_line = "%s%6.1s%10.4f%10.4f%10.4f%10.4f%10.4f\n" % ('CG1', 'X', 10.0, 10.2, 1.0, 1.5, 23.7)
-    mm_atom_prop_line = "%s%10.4f%10.4f%10.4f%10.4f\n" % ('CG1', -10.2, 10, -10.2, 10)
+    atom_prop_line = "%s%6.1s%10.4f%10.4f%10.4f%10.4f%10.4f\n" % ('CG1', 'X', 1.0, 0.2, 1.0, 1.5, 23.7)
+    mm_atom_prop_line = "%s%10.4f%10.4f%10.4f%10.4f\n" % ('CG1', -0.2, 1.0, -0.2, 1.0)
     
     # confirms changes in data dir
     assert atom_prop_line in fa_lines
@@ -38,27 +41,26 @@ def test_change_atom_parameters():
     assert atom_prop_line in pyrosetta_fa_lines
     assert mm_atom_prop_line in pyrosetta_mm_lines
 
-    pose = cg_pyrosetta.pyrosetta.pose_from_sequence('X[CG31:CGUpper]X[CG31:CGLower]')
-    sf = cg_pyrosetta.pyrosetta.ScoreFunction()
-    sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_inter_atr, 1)
-    sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_inter_rep, 1)
-    sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_intra_atr, 1)
-    sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_intra_rep, 1)
+def test_changing_score():
+    pose = pyrosetta.pose_from_sequence('X[CG11x3:CGLower]X[CG11x3:CGUpper]')
+    sf = pyrosetta.ScoreFunction()
+    sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_inter_atr, 1)
+    sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_inter_rep, 1)
+    sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_intra_atr, 1)
+    sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_intra_rep, 1)
 
     old_score = sf(pose)
     print("Old Score:", old_score)
-
     cg_pyrosetta.change_parameters.changeAtomParameters(
-                    {'CG1':['X', 1.0, 0.2, 1.0, 1.5, 23.7]})
+                    {'CG1':['X', 10.0, 10.2, 1.0, 1.5, 23.7]})
+    new_pose = pyrosetta.pose_from_sequence('X[CG11x3:CGLower]X[CG11x3:CGUpper]')
+    new_sf = pyrosetta.ScoreFunction()
+    new_sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_inter_atr, 1)
+    new_sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_inter_rep, 1)
+    new_sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_intra_atr, 1)
+    new_sf.set_weight(pyrosetta.rosetta.core.scoring.mm_lj_intra_rep, 1)
 
-    cg_pyrosetta.pyrosetta.init()
-    new_sf = cg_pyrosetta.pyrosetta.ScoreFunction()
-    new_sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_inter_atr, 1)
-    new_sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_inter_rep, 1)
-    new_sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_intra_atr, 1)
-    new_sf.set_weight(cg_pyrosetta.pyrosetta.rosetta.core.scoring.mm_lj_intra_rep, 1)
-
-    new_score = new_sf(pose)
+    new_score = new_sf(new_pose)
     print("New Score:", new_score)
 
     assert new_score !=  old_score
