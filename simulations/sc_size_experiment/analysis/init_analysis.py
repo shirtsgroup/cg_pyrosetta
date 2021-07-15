71815f77fc1695fcf585ac44e648d0aef9386f23
Theodore Fobe <tfobe@br013.ib.bridges2.psc.edu>
2021-07-15 13:43:53 -0400

    added clustering, energy trajectory and energy distributions scripts to signac workflow

--- simulations/sc_size_experiment/analysis/init_analysis.py
+++ simulations/sc_size_experiment/analysis/init_analysis.py
@@ -3,29 +3,15 @@ import numpy as np
 import pandas as pd
 import analyze_foldamers
 import os
-import argparse
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--signac_dir",
-        help="Directory containing signac project",
-        required=False,
-        type=str,
-        default=os.path.abspath(""),
-    )
-    
-    args = parser.parse_args()
-    return args
+import yaml
 
 def main():
-    args = parse_args()
-    original_path = os.path.abspath("")
+    stream = open("cluster_parameters.yml", "r")
+    params = yaml.safe_load(stream)
 
     # Getting Signac project schema
     print("Fetching signac schema information...")
-    os.chdir(args.signac_dir)
-    original_project = signac.get_project(root=args.signac_dir)
+    original_project = signac.get_project(root=params["signac_dir"])
     schema = original_project.detect_schema()
     sc_sizes, reps = schema.items()
     sc_sizes = list(sc_sizes[1][float])
@@ -33,12 +19,11 @@ def main():
     select_sc_sizes = [str(round(a, 3)) for a in sc_sizes]
 
     # Creating new signac proejct
-    os.chdir(original_path)
     project = signac.get_project()
 
     for sc in sc_sizes:
         print("New job:", sc)
-        job = project.open_job({'sc_size':sc, 'production_path':args.signac_dir})
+        job = project.open_job({'sc_size':sc})
         job.init()
 
 if __name__ == "__main__":
--- simulations/sc_size_experiment/analysis/signac_analysis.py
+++ simulations/sc_size_experiment/analysis/signac_analysis.py
@@ -7,16 +7,28 @@ import flow
 import matplotlib.pyplot as plt
 from flow import FlowProject
 import os
+import pickle
+import parser
+import yaml
 
+@FlowProject.label
+def energy_trajectory(job):
+    return job.isfile("energy_trajectory.pdf")
 
-def parse_args():
-    pass
+# @FlowProject.label
+# def clustering(job):
+#     if len(job.stores['clustering']['labels']) > 0:
+#         return True
+#     else:
+#         return False
 
-@flow.directives(fork=True)
+@flow.directives(fork=False)
 @FlowProject.operation
 @FlowProject.post.isfile("energy_trajectory.pdf")
+@FlowProject.post(energy_trajectory)
 def get_energy_trajectory(job):
-    old_project = signac.get_project(root=job.sp.production_path)
+    param_dict = get_clustering_parameters(job)
+    old_project = signac.get_project(root=param_dict["signac_dir"])
 
     # kT values for tempeature plot
     kts = np.array([10 * ( 0.9 ) ** i for i in range(50)])
@@ -49,8 +61,123 @@ def get_energy_trajectory(job):
 
     fig.savefig(job.fn("energy_trajectory.jpg"), bbox_inches="tight")
     fig.savefig(job.fn("energy_trajectory.pdf"), bbox_inches="tight")
+    
+    with open(job.fn('traj_files.pkl'),'wb') as fp:
+        pickle.dump(traj_file_list, fp)
+    job.stores['traj']['energy_traj'] = np.array(energy_traj)
+    job.stores['traj']['all_energies'] = all_energies
+    
 
     plt.close("all")
+
+def get_default_parameters():
+    default_params = {
+        "eps": 0.15,
+        "min_samples": 30,
+        "frame_start": 700,
+        "frame_stride": 3,
+        "frame_end": -1,
+        "output_format": "pdb",
+        "plot_silhouette": True,
+        "filter": True,
+        "filter_ratio": 0.5,
+        "output_cluster_traj": True,
+        "core_points_only": False,
+        "parallel" : -1,
+    }
+    return default_params
+
+def get_clustering_parameters(job):
+    if os.path.isfile(job.fn("cluster_parameters.yml")):
+        param_dict = read_yaml_params(job.fn("cluster_parameters.yml"))
+    else:
+        if os.path.isfile("cluster_parameters.yml"):
+            param_dict = read_yaml_params("cluster_parameters.yml")
+        else:
+            param_dict = get_default_parameters()
+    return param_dict
+
+
+def read_yaml_params(filename):
+    stream = open(filename, "r")
+    params = yaml.safe_load(stream)
+    return params
+
+@flow.directives(fork=False)
+@FlowProject.operation
+@FlowProject.pre(energy_trajectory)
+@FlowProject.post.isfile("clustering.h5")
+def run_clustering(job):
+
+    # Run clustering and store output in job.data
+
+    param_dict = get_clustering_parameters(job)
+    param_dict.pop("signac_dir", None)
+
+    with open(job.fn('traj_files.pkl'), 'rb') as fp:
+        traj_file_list = pickle.load(fp)
+    (
+        medoid_positions,
+        cluster_sizes,
+        cluster_rmsd,
+        n_noise,
+        silhouette_avg,
+        labels,
+        original_indices,
+    ) = analyze_foldamers.cluster.get_cluster_medoid_positions_DBSCAN(
+        traj_file_list,
+        None,
+        output_dir=job.fn("cluster_output"),
+        **param_dict
+    )
+
+    job.stores['clustering']['medoid_positions'] = medoid_positions
+    job.stores['clustering']['cluster_sizes'] = cluster_sizes
+    job.stores['clustering']['cluster_rmsd'] = cluster_rmsd
+    job.stores['clustering']['n_noise'] = n_noise
+    job.stores['clustering']['silhouette_avg'] = silhouette_avg
+    job.stores['clustering']['labels'] = labels
+    job.stores['clustering']['original_indices'] = original_indices
+
+
+@flow.directives(fork=False)
+@FlowProject.operation
+@FlowProject.pre.after(run_clustering)
+@FlowProject.post.isfile("cluster_energy_dist_cluster_0.pdf")
+def cluster_energy_distributions(job):
+    if len(job.stores['clustering']['cluster_sizes']) > 0:
+        # Output cluster energy distributions
+        clusters = list(np.unique(job.stores['clustering']['labels']))
+        all_cluster_energies = []
+        if -1 in clusters:
+            clusters.remove(-1)
+        for i in clusters:
+            plt.figure(figsize=[5, 5], dpi=100)
+            cluster_indices = np.where(job.stores['clustering']['labels'] == i)[0]
+            cluster_energies = all_energies[job.stores['clustering']['original_indices'][cluster_indices]]
+            all_cluster_energies.append(cluster_energies)
+            mean_energy = np.mean(cluster_energies)
+            std_energy = np.std(cluster_energies)
+            color = cm.nipy_spectral(float(i) / len(clusters))
+            plt.hist(cluster_energies, bins=50, color="red")
+            plt.xlabel("Energy (A.E.U)")
+            plt.ylabel("Counts")
+            plt.title("Cluster " + str(i) + " Energy Distribution")
+            plt.savefig(
+                os.path.join(
+                    sub_dir, "cluster_energy_dist_cluster_" + str(i) + ".pdf"
+                ), bbox_inches="tight"
+            )
+            plt.savefig(
+                os.path.join(
+                    sub_dir, "cluster_energy_dist_cluster_" + str(i) + ".jpg"
+                ), bbox_inches="tight"
+            )
+            plt.close("all")
+
+
+
+    
 
 
 
